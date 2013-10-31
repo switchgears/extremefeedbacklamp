019983b2da64795d9a486d88bc8846b6e4bd1b59
Aske Olsson <aske.olsson@switch-gears.dk>
2013-10-31 14:54:29 +0100

    Change owner for extremefeedbacklamp dir after updating
    Change-Id: Ie690982895a8f53f50b956da54401f4fc5ff60fa
    Signed-off-by: Aske Olsson <aske.olsson@switch-gears.dk>

--- xfdlampupdate.sh
+++ xfdlampupdate.sh
@@ -9,6 +9,7 @@ then
     echo "update found and applied succesfully, rebooting"
     sudo cp -f /home/pi/extremefeedbacklamp/xfdlampupdate.sh /etc/cron.daily/xfdlampupdate
     sudo chmod +x /etc/cron.daily/xfdlampupdate
+    sudo chown -R pi:pi /home/pi/extremefeedbacklamp
     #sudo cp -f /home/pi/extremefeedbacklamp/xfdlampupdate.sh /etc/cron.hourly/xfdlampupdate
     #sudo chmod +x /etc/cron.hourly/xfdlampupdate
     sudo shutdown -r now
