953a6afaf83703a54f2d0d7264297e783a1db070
Knud Poulsen <knud.poulsen@switch-gears.dk>
2015-09-03 12:58:33 +0200

    autoupdate, ordering of events

--- xfdlampupdate.sh
+++ xfdlampupdate.sh
@@ -4,11 +4,13 @@ git remote update || true
 git fetch || true
 DELTAPRE=$(git rev-list HEAD...origin/master --count)
 sudo git reset --hard origin/master || true
-sudo git clean -xfd || true
 DELTAPOST=$(git rev-list HEAD...origin/master --count)
 if [ "$DELTAPRE" != "$DELTAPOST" ]
 then
     echo "updates found ... rerunning install ..."
+    sudo git clean -xfd || true
+    sudo git reset --hard origin/master || true
+    sudo git clean -xfd || true
     sudo ln -sf /home/pi/extremefeedbacklamp/xfdlampupdate.sh /etc/cron.daily/xfdlampupdate
     sudo chmod +x /etc/cron.daily/xfdlampupdate
     sudo chown -R pi:pi /home/pi/extremefeedbacklamp
